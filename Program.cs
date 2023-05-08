92d149358cbe092e5526baf059e1774df5e3b989
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-05-09 01:24:13 +0500

    Currency_Converter_5

--- Program.cs
+++ Program.cs
@@ -11,18 +11,22 @@ namespace Currency_Converter
     {
         static void Main(string[] args)
         {
-            const string optionОne = "1";
-            const string optionTwo = "2";
-            const string optionThree = "3";
-            const string optionFour = "4";
-            const string optionFive = "5";
-            const string optionSix = "6";
-            const string optionEight = "7";
+            const string RurToUsd = "1";
+            const string RurToCnh = "2";
+            const string UsdToRur = "3";
+            const string UsdToCnh = "4";
+            const string CnhToRur = "5";
+            const string CnhToUsd = "6";
+            const string IsExit = "7";
+
 
             double rurCount = 1000, usdCount = 1000, cnhCount = 1000;
             double usdRur = 76.82;
+            double rurUsd = 1 / usdRur;
             double usdCnh = 6.91;
+            double cnhUsd = 1/ usdCnh;
             double cnhRur = 11.12;
+            double rurCnh = 1/ cnhRur;
             double purchaseVolume;
             bool isExit = false;
             string userInput;                      
@@ -35,53 +39,53 @@ namespace Currency_Converter
                 string programMenu = $"На вашем счету {rurCount} рублей, {usdCount} долларов,{cnhCount} юаней." +
                                      $"\nКурс валют USD/RUR: {usdRur}, USD/CNH: {usdCnh}, CNH/RUR: {cnhRur}." +
                                      $" Операции на выбор:" +
-                                     $"\n1 - обменять рубли на доллары. 2 - обменять рубли на юани." +
-                                     $"\n3 - обменять доллраны на рубли. 4 - обменять доллары на юани." +
-                                     $"\n5 - обмен юани на рубли. 6 - обмен юаней на доллары." +
-                                     $"\n7 - закрыть программу";
+                                     $"\n{RurToUsd} - обменять рубли на доллары. {RurToCnh} - обменять рубли на юани." +
+                                     $"\n{RurToCnh} - обменять доллраны на рубли. {UsdToCnh} - обменять доллары на юани." +
+                                     $"\n{CnhToRur} - обмен юани на рубли. {CnhToUsd} - обмен юаней на доллары." +
+                                     $"\n{IsExit} - закрыть программу";
 
                 Console.WriteLine(programMenu);
                 userInput = Console.ReadLine();           
 
                 switch (userInput)
                 {
-                    case optionОne:
+                    case RurToUsd:
                         Console.WriteLine(requestPurchaseVolume);
                         purchaseVolume = (float)Convert.ToDouble(Console.ReadLine());
                         rurCount -= purchaseVolume;
-                        usdCount += purchaseVolume / usdRur;
+                        usdCount += purchaseVolume * rurUsd;
                         break;
-                    case optionTwo:
+                    case RurToCnh:
                         Console.WriteLine(requestPurchaseVolume);
                         purchaseVolume = (float)Convert.ToDouble(Console.ReadLine());
                         rurCount -= purchaseVolume;
-                        cnhCount += purchaseVolume / cnhRur;
+                        cnhCount += purchaseVolume * rurCnh;
                         break;
-                    case optionThree:
+                    case UsdToRur:
                         Console.WriteLine(requestPurchaseVolume);
                         purchaseVolume = (float)Convert.ToDouble(Console.ReadLine());
                         usdCount -= purchaseVolume;
                         rurCount += purchaseVolume * usdRur;
                         break;
-                    case optionFour:
+                    case UsdToCnh:
                         Console.WriteLine(requestPurchaseVolume);
                         purchaseVolume = (float)Convert.ToDouble(Console.ReadLine());
                         usdCount -= purchaseVolume;
-                        cnhCount += purchaseVolume / usdCnh;
+                        cnhCount += purchaseVolume * usdCnh;
                         break;
-                    case optionFive:
+                    case CnhToRur:
                         Console.WriteLine(requestPurchaseVolume);
                         purchaseVolume = (float)Convert.ToDouble(Console.ReadLine());
                         cnhCount -= purchaseVolume;
                         rurCount += purchaseVolume * cnhRur;
                         break;
-                    case optionSix:
+                    case CnhToUsd:
                         Console.WriteLine(requestPurchaseVolume);
                         purchaseVolume = (float)Convert.ToDouble(Console.ReadLine());
                         cnhCount -= purchaseVolume;
-                        usdCount += purchaseVolume / usdCnh;
+                        usdCount += purchaseVolume * cnhUsd;
                         break;
-                    case optionEight:
+                    case IsExit:
                         Console.WriteLine(exitProgramMessege);
                         isExit = true;
                         break;
